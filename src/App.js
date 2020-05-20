86c584920217198f6a253449ee338419c1043650
Sérgio Toledo <contato@sergiotoledo.com.br>
2020-05-20 18:11:39 -0300

    Change real data to mock

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     if (window.RDStationForms) {
       // Essa linha cria o elemento do formulário e faz o append do conteúdo na div abaixo.
       // Substitua os parâmetros com os dados do seu formulário.
-      new window.RDStationForms('teste-caldas-ea31f26de172d1e9c5b6', 'UA-17276574-1').createForm()
+      new window.RDStationForms('my-form-aeiou6de172d1e9c5b6', 'UA-36276574-1').createForm();
     }
   }
 
@@ -17,7 +17,7 @@ class App extends Component {
           <p>Formulário integrado RDSM<br />Dentro de um React App</p>
 
           {/* Esta div é necessária para receber o conteúdo do formulário. Substitua os parâmetros com os dados do seu formulário. */}
-          <div role="main" id="teste-caldas-ea31f26de172d1e9c5b6"></div>
+          <div role="main" id="my-form-aeiou6de172d1e9c5b6"></div>
         </header>
       </div>
     );
