acdcda962e3f91fdef450eabfc639462fdddaeb2
Lucas Zunder <lucas.zunder@hotmail.com>
2020-05-28 15:11:23 -0300

    fix export syntax

--- src/components/monitoring-code-form-hooks.js
+++ src/components/monitoring-code-form-hooks.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-export const MonitoringCodeFormHooks = () => {
+const MonitoringCodeFormHooks = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
 
